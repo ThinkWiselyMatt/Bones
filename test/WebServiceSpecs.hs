de2b4ec3567c367369e5f38d29bd824cfea9de4d
M <thinkwiselymatt@gmail.com>
2024-07-07 17:02:24 -0500

    All 3 tests pass with logging suppressed but log file still empty

--- test/WebServiceSpecs.hs
+++ test/WebServiceSpecs.hs
@@ -7,10 +7,12 @@ import Test.Hspec
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import Control.Concurrent (threadDelay)
-import System.Process (createProcess, proc, terminateProcess, waitForProcess, ProcessHandle)
+import Control.Concurrent (threadDelay, forkIO, killThread, ThreadId)
+import System.IO (hGetContents, hPutStr, hPutStrLn, stderr, openFile, IOMode(WriteMode), hFlush)
+import System.Process (createProcess, proc, terminateProcess, waitForProcess, ProcessHandle, StdStream(CreatePipe), std_out, std_err)
 import Data.Text (isInfixOf)
 import Data.Text.Encoding (decodeUtf8)
+import Control.Monad (when, void)
 
 spec :: Spec
 spec = beforeAll startServices $ afterAll stopServices $ do
@@ -34,15 +36,27 @@ spec = beforeAll startServices $ afterAll stopServices $ do
       response <- httpLbs request manager
       L8.unpack (responseBody response) `shouldBe` "Hello from Servant!"
 
-startServices :: IO ProcessHandle
+startServices :: IO (ProcessHandle, [ThreadId])
 startServices = do
-  (_, _, _, ph) <- createProcess (proc "stack" ["exec", "bones-exe"])
+  (_, Just hout, Just herr, ph) <- createProcess (proc "stack" ["exec", "bones-exe"])
+    { std_out = CreatePipe, std_err = CreatePipe }
+  -- Capture and log the output if needed
+  let logOutput = False -- Set this to True if you want to see logs
+  logFile <- openFile "test_output.log" WriteMode
+  let handleOutput handle = forkIO $ do
+        contents <- hGetContents handle
+        if logOutput
+          then mapM_ (hPutStrLn stderr) (lines contents)
+          else hPutStr logFile contents >> hFlush logFile
+  outThread <- handleOutput hout
+  errThread <- handleOutput herr
   -- Give the services some time to start
   threadDelay 5000000  -- 5 seconds
-  return ph
+  return (ph, [outThread, errThread])
 
-stopServices :: ProcessHandle -> IO ()
-stopServices ph = do
+stopServices :: (ProcessHandle, [ThreadId]) -> IO ()
+stopServices (ph, threads) = do
+  mapM_ killThread threads
   terminateProcess ph
   _ <- waitForProcess ph
   return ()
