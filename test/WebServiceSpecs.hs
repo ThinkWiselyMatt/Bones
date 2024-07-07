d9f9159585c6d6d610b4d09ff23dc2f750f9f375
M <thinkwiselymatt@gmail.com>
2024-07-07 17:15:06 -0500

    This fucking log file better get some content soon

--- test/WebServiceSpecs.hs
+++ test/WebServiceSpecs.hs
@@ -8,14 +8,14 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Control.Concurrent (threadDelay, forkIO, killThread, ThreadId)
-import System.IO (hGetContents, hPutStr, hPutStrLn, stderr, openFile, IOMode(WriteMode), hFlush)
+import System.IO (hGetContents, hPutStr, hPutStrLn, stderr, openFile, IOMode(WriteMode), IOMode(AppendMode), hFlush, Handle, hClose)
 import System.Process (createProcess, proc, terminateProcess, waitForProcess, ProcessHandle, StdStream(CreatePipe), std_out, std_err)
 import Data.Text (isInfixOf)
 import Data.Text.Encoding (decodeUtf8)
 import Control.Monad (when, void)
 
 spec :: Spec
-spec = beforeAll startServices $ afterAll stopServices $ do
+spec = beforeAll (startServices logOutput) $ afterAll (stopServices logOutput) $ do
   describe "Web Service Apps" $ do
     it "responds from Scotty app" $ \_ -> do
       manager <- newManager tlsManagerSettings
@@ -36,15 +36,17 @@ spec = beforeAll startServices $ afterAll stopServices $ do
       response <- httpLbs request manager
       L8.unpack (responseBody response) `shouldBe` "Hello from Servant!"
 
-startServices :: IO (ProcessHandle, [ThreadId])
-startServices = do
+logOutput :: Bool
+logOutput = False -- Set this to True if you want to see logs
+
+startServices :: Bool -> IO (ProcessHandle, [ThreadId], Handle)
+startServices logOutput = do
+  logFile <- openFile "test_output.log" AppendMode -- This will append to the file (doesnt work still empty log file TODO)
   (_, Just hout, Just herr, ph) <- createProcess (proc "stack" ["exec", "bones-exe"])
     { std_out = CreatePipe, std_err = CreatePipe }
-  -- Capture and log the output if needed
-  let logOutput = False -- Set this to True if you want to see logs
-  logFile <- openFile "test_output.log" WriteMode
   let handleOutput handle = forkIO $ do
         contents <- hGetContents handle
+        putStrLn "Reading output from handle..." -- Debug statement
         if logOutput
           then mapM_ (hPutStrLn stderr) (lines contents)
           else hPutStr logFile contents >> hFlush logFile
@@ -52,11 +54,13 @@ startServices = do
   errThread <- handleOutput herr
   -- Give the services some time to start
   threadDelay 5000000  -- 5 seconds
-  return (ph, [outThread, errThread])
+  return (ph, [outThread, errThread], logFile)
 
-stopServices :: (ProcessHandle, [ThreadId]) -> IO ()
-stopServices (ph, threads) = do
+stopServices :: Bool -> (ProcessHandle, [ThreadId], Handle) -> IO ()
+stopServices logOutput (ph, threads, logFile) = do
   mapM_ killThread threads
   terminateProcess ph
   _ <- waitForProcess ph
+  hFlush logFile
+  hClose logFile
   return ()
