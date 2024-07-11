ecc6cffec41d6a8e6ab9c4662b6a05edfd51ad88
M <thinkwiselymatt@gmail.com>
2024-07-10 22:59:18 -0500

    log file finally has stuff in it + quickcheck test for add does 5 calls with random numbers
    why the FUCK is it starting up twice it should only be starting up once

--- test/QuickCheckSpecs.hs
+++ test/QuickCheckSpecs.hs
@@ -4,11 +4,15 @@
 module QuickCheckSpecs (spec) where
 
 import Test.Hspec
+import Test.QuickCheck
 import Network.HTTP.Client (newManager, defaultManagerSettings, httpLbs, parseRequest, responseBody, Manager)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Char8 as S8
 import Control.Concurrent (threadDelay)
 import Control.Exception (try, SomeException)
+import Text.Read (readMaybe)
+import Data.List (isPrefixOf, find, isInfixOf, stripPrefix)
+import Data.Char (isSpace)
 
 
 spec :: Spec
@@ -22,10 +26,34 @@ spec = describe "QuickCheck tests for all endpoints" $ do
       manager <- newManager defaultManagerSettings
       responses <- retry 3 $ mapM (makeRequest manager) baseEndpoints
       all (S8.isInfixOf "Hello from C#" . L8.toStrict) responses `shouldBe` False
+
+    it "quickcheck tests for /cpp/add/x/y endpoints" $ withMaxSuccess 5 $ property $ \x y -> do
+      manager <- newManager defaultManagerSettings
+      results <- mapM (testCppAddEndpoint manager x y) baseEndpoints
+      all id results `shouldBe` True
     
     it "Service still running if you want to test anything in browser -- hit enter to continue" $ \_ -> do
       (1 + 1) `shouldBe` 2
 
+contains :: String -> String -> Bool
+contains = isInfixOf
+
+testCppAddEndpoint :: Manager -> Int -> Int -> String -> IO Bool
+testCppAddEndpoint manager x y url = do
+  let fullUrl = url ++ "/cpp/add/" ++ show x ++ "/" ++ show y
+  putStrLn $ "Calling URL: " ++ fullUrl
+  request <- parseRequest fullUrl
+  response <- httpLbs request manager
+  let body = L8.unpack (responseBody response)
+  putStrLn $ "Response: " ++ body
+  let sumResult = parseSum body
+  putStrLn $ "Parsed result: " ++ show sumResult
+  return $ case sumResult of
+    Just result -> result == x + y
+    Nothing -> False
+
+parseSum :: String -> Maybe Int
+parseSum body = readMaybe . reverse . takeWhile (/= '=') . dropWhile isSpace . reverse $ body --works backwards dropping whitespace, until it gets to equals sign to just grab the num
 
 makeRequest :: Manager -> String -> IO L8.ByteString
 makeRequest manager url = do
@@ -47,6 +75,9 @@ baseEndpoints =
   , "http://localhost:3002/yesod"  
   , "http://localhost:3003/servant"  
   ]
+
+arbitraryValue :: Gen Int
+arbitraryValue = choose (-10000, 10000)
 
 retry :: forall a. Int -> IO a -> IO a
 retry n action
