ba6fdab98034baa095e490c5155f7dc135126486
M <thinkwiselymatt@gmail.com>
2024-07-11 22:43:42 -0500

    OS specific build scripts since pathing is different

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+set LIBRARY_PATH=C:\Users\Owner\Desktop\Development\Bones\native
+set INCLUDE_PATH=C:\Users\Owner\Desktop\Development\Bones\native
+stack build
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LIBRARY_PATH=/home/matt/DEV/Bones/native
+export INCLUDE_PATH=/home/matt/DEV/Bones/native
+stack build
