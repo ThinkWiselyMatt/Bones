e938de2f88b50b61fc48fa617d4289d13570253a
M <thinkwiselymatt@gmail.com>
2024-07-13 00:44:20 -0500

    Revert "OS specific build scripts since pathing is different"
    This reverts commit ba6fdab98034baa095e490c5155f7dc135126486.

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set LIBRARY_PATH=C:\Users\Owner\Desktop\Development\Bones\native
-set INCLUDE_PATH=C:\Users\Owner\Desktop\Development\Bones\native
-stack build
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_PATH=/home/matt/DEV/Bones/native
-export INCLUDE_PATH=/home/matt/DEV/Bones/native
-stack build
